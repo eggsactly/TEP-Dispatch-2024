419c93ab1b548e5b56948e5e91dad32cc7c42a16
Garrett Weaver <wagarrettweaver@gmail.com>
2025-12-30 01:36:57 -0700

    Added weekly energy use to plot 2024 dc energy use.

--- plot-2024-dc-energy-use.py
+++ plot-2024-dc-energy-use.py
@@ -52,6 +52,7 @@ with open('TEP-Dispatch-2024.csv', newline='') as csvfile:
     labels = []
     # initialize hourlyEnergyUse to be a 24 element array of elements 
     hourlyEnergyUse = []
+    hourlyEnergyUse2 = []
     count = 0
     while count < hoursInADay:
         hourlyEnergyUse.append([])
@@ -62,7 +63,30 @@ with open('TEP-Dispatch-2024.csv', newline='') as csvfile:
     count = 0
     while count < len(energyImports['temp']):
         hourlyEnergyUse[count % hoursInADay].append(dataCenterEnergyUse(energyImports['temp'][count]))
+        hourlyEnergyUse2.append(dataCenterEnergyUse(energyImports['temp'][count]))
         count = count + 1 
+    
+    print("max(hourlyEnergyUse2)" + str(max(hourlyEnergyUse2)))
+    
+    maxEnergyUse = float(max(hourlyEnergyUse2))
+    aveEnergyUse = float(sum(hourlyEnergyUse2) / float(len(hourlyEnergyUse2)))
+    
+    print("Yearly Max Energy Use (MWh): " + str(maxEnergyUse))
+    print("Yearly Ave Energy Use (MWh): " + str(aveEnergyUse)) 
+    
+    print("Yearly Load Factor: " + str(aveEnergyUse/maxEnergyUse))
+    
+    averageHourlyUse = []
+    for hour in hourlyEnergyUse:
+        averageHourlyUse.append(float(sum(hour) / float(len(hour))))
+    
+    maxEnergyUse = float(max(averageHourlyUse))
+    aveEnergyUse = float(sum(averageHourlyUse) / float(len(averageHourlyUse)))
+    
+    print("Weekly Max Energy Use (MWh): " + str(maxEnergyUse))
+    print("Weekly Ave Energy Use (MWh): " + str(aveEnergyUse)) 
+    
+    print("Weekly Load Factor: " + str(aveEnergyUse/maxEnergyUse))
     
     fruit_weights = hourlyEnergyUse
 
